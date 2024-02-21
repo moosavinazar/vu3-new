19d83fed7b579792d6140824fa14b06c2f15c9b1
Moosavi <s.ha.mousavi.nazar@gmail.com>
2024-02-21 08:27:04 +0330

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2 @click="increment">{{ count }}</h2>
+  <Home title="Home Page" @increment-event="increment"/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+  import { ref } from 'vue';
+  import Home from './components/Home.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  const count = ref(0);
+    
+  function increment(value) {
+    count.value += value;
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>{{ props.title }}</h2>
+  <!-- <button @click="$emit('incrementEvent', 3)">increment</button> -->
+  <button @click="sumevent">increment</button>
+</template>
+
+<script setup>
+
+    import { defineProps, defineEmits } from 'vue';
+
+    const props = defineProps(['title']);
+    const emit = defineEmits(['incrementEvent']);
+
+    function sumevent() {
+        emit('incrementEvent', 3);
+    }
+
+</script>
+
+<style>
+
+</style>
